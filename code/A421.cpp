b7205a2839f8ac660cf2ff38355b50d098911bca
bakrbasser <zedalbser123@gmail.com>
2025-03-04 21:27:06 +0200

    code/A421.cpp

---
+++ code/A421.cpp
@@ -0,0 +1,23 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+        int missingNumber(vector<int> &nums)
+        {
+                double totalSum = nums.size() / 2.0 * (nums.size() + 1);
+                for (int i = 0; i < nums.size(); i++)
+                {
+                        totalSum -= nums[i];
+                }
+                return totalSum;
+        }
+};
+
+int main()
+{
+        vector<int> arr = {3, 1, 0};
+        Solution G;
+        cout << G.missingNumber(arr);
+}
